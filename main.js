5db4a6dd2677500aa7f2108e37283a51f3933d1e
nasimua <nasimua@hotmail.com>
2022-10-21 15:10:06 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,120 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+
+  toggleCheckOutStatus() {
+    this.isCheckedOut = !this.isCheckOut;
+  }
+
+  getAverageRating() {
+    let ratingsSum = this.ratings.reduce((acc, rating) => acc + rating);
+
+    return ratingsSum / this.ratings.length;
+  }
+
+  addRating(value) {
+    this.ratings.push(value);
+  }
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+class CD extends Media {
+  constructor(artist, title, songs) {
+    super(title);
+    this._artist = artist;
+    this._songs = songs;
+  }
+
+  get artist() {
+    return this._artist;
+  }
+  get songs() {
+    return this._songs;
+  }
+}
+
+const historyOfEverything = new Book(
+  "Bill Bryson",
+  "A Short History of Nearly Everything",
+  544
+);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+// console.log(historyOfEverything.pages);
+
+const speed = new Movie("Jan de Bont", "Speed", 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
+
+const thriller = new CD("Michael Jackson", "Thriller", [
+  "Wanna Be Startin Somethin",
+  "Baby Be Mine",
+  "The Girl Is Mine",
+  "Thriller",
+  "Beat It",
+  "Billie Jean",
+  "Human Nature",
+  "P.Y.T. (Pretty Young Thing)",
+  "The Lady In My Life",
+]);
+thriller.toggleCheckOutStatus();
+console.log(thriller.isCheckedOut);
+thriller.addRating(4);
+thriller.addRating(4);
+thriller.addRating(5);
+console.log(thriller.getAverageRating());
+// console.log(thriller.songs)
